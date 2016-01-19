eff4131c94399ddec8f3c1b2b8aefab6b4415413
irusiabondarenko <irusiabondarenko@gmail.com>
2016-01-19 00:24:33 -0800

    Valid binary Chrismas Tree

---
+++ Tree - ValidBinaryChrismasTree.js
@@ -0,0 +1,29 @@
+function isValidTree(tree){
+
+   if (tree.ornament!=="star") {
+     return false;
+   } 
+   var left = tree.left;
+   var right = tree.right;
+   
+   function process(tree) {
+       if(!tree){return;}
+       if (!tree.left&&!tree.right) {
+         if (tree.color!=="blue") {
+           return false;
+         }
+       }
+       if (tree.left||tree.right) {
+         if (tree.color!=="red") {
+           return false;
+         } 
+       }
+       if((process(tree.left)==false)||(process(tree.right)==false)) {
+         return false;
+       }
+    }
+   if((process(left)==false)||(process(right)==false)) {
+     return false;
+   }
+    return true;
+}
