6c00998e08e5d8de7ed9c0fd25a293251517a3a4
irusiabondarenko <irusiabondarenko@gmail.com>
2016-02-12 15:31:27 -0800

    Added solved problem

---
+++ Trees - Convert sorted linked list BST.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function(head) {
+    if (head===null) {
+        return null;
+    }
+    var cur = head;
+    var length = getLength(head);
+    return BST(0, length-1);
+    
+        function getLength (head) {
+            var count = 0;
+            var cur = head;
+            while (cur) {
+                count++;
+                cur = cur.next;
+            }
+            return count;
+        }
+        
+        
+        function BST(start, end) {
+            if (start>end) {
+                return null;
+            }
+            var mid = Math.floor((start+end)/2);
+            var left = BST (start, mid-1);
+            var root = new TreeNode(cur.val)
+            cur = cur.next;
+            var right = BST (mid+1, end);
+            root.left = left;
+            root.right = right;
+            return root;
+        }
+};
+
