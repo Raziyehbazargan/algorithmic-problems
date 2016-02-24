a78f87f96128d5a2e166e1e087cc879dcf7afa93
irusiabondarenko <irusiabondarenko@gmail.com>
2016-02-24 15:53:57 -0800

    Added problems

---
+++ Linked Lists - Cycle in linked list.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (head===null) {
+        return false;
+    }
+   var slow, fast;
+   slow = fast = head;
+    
+    while (true) {
+        slow = slow.next;
+        if (fast.next!==null) {
+            fast = fast.next.next;
+        } else {
+            return false;
+        }
+        if (slow===null||fast===null) {
+            return false;
+        }
+        if (slow===fast) {
+            return true;
+        }
+    }
+};
