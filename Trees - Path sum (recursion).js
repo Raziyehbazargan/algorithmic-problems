0c8596817f1b98696d58dd6b12914efd2d86ec7e
irusiabondarenko <irusiabondarenko@gmail.com>
2016-03-01 15:22:06 -0800

    Added problems

--- Trees - Path sum (recursion).js
+++ Trees - Path sum (recursion).js
@@ -19,3 +19,38 @@ var hasPathSum = function(root, sum) {
     }
     return hasPathSum (root.left, sum - root.val)|| hasPathSum (root.right, sum - root.val);
 };
+
+//second solution (print this path);
+
+function Tree (val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+var root = new Tree(10);
+root.left = new Tree(8);
+root.right = new Tree(12);
+root.left.left = new Tree(6);
+
+function pathSum (root, sum, result) {
+	if (root===null) {
+		return null;
+	}
+	if (root.right===null&&root.left===null) {
+		if (root.val===sum) {
+			result.push(root.val);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	if (pathSum (root.left, sum-root.val, result)){
+		result.push(root.val);
+		return true;
+	}
+	if (pathSum(root.right, sum-root.val, result)) {
+		result.push(root.val);
+		return true;
+	}
+	return false;
+}
