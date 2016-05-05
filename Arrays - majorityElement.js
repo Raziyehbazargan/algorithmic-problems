1a5b8aa93f44cd1551035941053f15f32b881352
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-05-05 07:08:34 -0700

    Solved majority element problem

--- Arrays - majorityElement.js
+++ Arrays - majorityElement.js
@@ -2,22 +2,42 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+
 var majorityElement = function(arr) {
+    if (arr.length===0) {
+        return [];
+    }
     if (arr.length===1) {
         return arr[0];
     }
-    var map = {};
-    for (var i=0; i<arr.length; i++) {
-        if (!map[arr[i]]) {
-            map[arr[i]]=1;
+    var count = 1;
+    var index = 0;
+    for (var i=1; i<arr.length; i++) {
+        if (arr[i]===arr[index]) {
+            count++;
         } else {
-            map[arr[i]]++;
+            count--;
+        }
+        if (count===0) {
+            index=i;
+            count=1;
         }
     }
-    for (var num in map) {
-        if (map[num]>arr.length/2) {
-            return parseInt(num);
-        }
-    }
+    return isMajority(index, arr);
+    
 };
 
+function isMajority (index, arr) {
+    var count =0;
+    for (var i=0; i<arr.length;i++) {
+        if (arr[index]===arr[i]) {
+            count++;
+        }
+    }
+    if (count>arr.length/2) {
+        return arr[index];
+    } else {
+        return false;
+    }
+}
