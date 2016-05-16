a35d81d2711f81cb0598549ea5c9898720791d21
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-05-15 19:36:47 -0700

    Convert decimal to roman

--- RomanNumbers.js
+++ RomanNumbers.js
@@ -11,3 +11,16 @@ function convertRomanToNumeric (romanNumber) {
 	}
 	return converted;
 }
+
+function convertNumericToRoman (number) {
+	var decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+	var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X","IX", "V", "IV", "I"];
+	var converted = "";
+	for (var i =0; i<roman.length; i++) {
+		while (number>=decimal[i]) {
+			converted+=roman[i];
+			number-=decimal[i];
+		}
+	}
+	return converted;
+}
