e453f8a2e102b72965dae3c3a4d133d31f27158c
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-05-23 13:02:51 -0700

    Added problem

--- Strings - isPolindrom.js
+++ Strings - isPolindrom.js
@@ -13,3 +13,23 @@ function isPolindrom (str) {
 	}
 	return true;
 }
+
+function isPolindrom (str) {
+	str = str.split("");
+    var i,j;
+	if (str.length%2!==0) {
+		i = j = Math.floor(str.length/2);
+	} else {
+		i = str.length/2-1;
+		j = i+1;
+	}
+
+	while (i>=0) {
+		if (str[i]!==str[j]) {
+			return false;
+		};
+		i--;
+		j++;
+	}
+	return true;
+}
