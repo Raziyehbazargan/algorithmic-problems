548bc02f12d9b4ba71690c6c1ce1d36cfea4d80c
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-08-07 21:08:42 -0700

    Solution using merge sort

--- Arrays - Sum of lowest two integers.js
+++ Arrays - Sum of lowest two integers.js
@@ -23,3 +23,31 @@ function sumTwoSmallestNumbers(numbers) {
   }
   return bigger+smaller;
 };
+
+//using merge sort
+
+function sumTwoSmallestNumbers(numbers) {  
+  var sortedArray = mergeSort(numbers);
+  return sortedArray[0] + sortedArray[1];
+  function mergeSort(arr) {
+    if (arr.length<2) {
+      return arr;
+    }
+    var mid = Math.floor(arr.length/2);
+    var left = mergeSort(arr.slice(0, mid));
+    var right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+  };
+
+  function merge(left, right) {
+    var result = [];s
+    while (left.length>0&&right.length>0) {
+      if (left[0]<right[0]) {
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
+    }
+    return result.concat(left.length? left : right);
+  }
+}
